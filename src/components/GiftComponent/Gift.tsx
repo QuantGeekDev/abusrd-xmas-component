cd54bad32c10f6a233aeb06cd92ea181551947ee
Alex Andru <alex007d@gmail.com>
2024-01-06 20:49:57 +0100

    Add Scratching effect

--- src/components/GiftComponent/Gift.tsx
+++ src/components/GiftComponent/Gift.tsx
@@ -1,14 +1,63 @@
-import GiftStyled from "./GiftStyled"
+import { useRef, useState, useEffect } from "react";
+import GiftStyled from "./GiftStyled";
 
 const Gift = (): React.ReactElement => {
-return (
-<GiftStyled className="gift">
+  const [isScratching, setIsScratching] = useState(false);
 
-<img className="gift__giftbox" height="100" width="100" src="/images/giftBox.png"/>
-<img className="gift__blowfish" height="100" width="100" src="/images/blowfish.png"/>
+  const giftReference = useRef<HTMLDivElement>(null);
+  const canvasReference = useRef<HTMLCanvasElement>(null);
 
-</GiftStyled>
-)
+  const handleMouseDown = () => {
+    setIsScratching(true);
+  };
 
-}
-export default Gift
+  const handleMouseUp = () => {
+    setIsScratching(false);
+  };
+
+  const handleMouseMove = (event: React.MouseEvent) => {
+    const scratchTraceHeight = 5;
+    const scratchTraceWidth = 5;
+    if (!isScratching || !canvasReference.current) return;
+
+    const canvasContext = canvasReference.current.getContext('2d');
+    if (canvasContext) {
+      const rect = canvasReference.current.getBoundingClientRect();
+      const xPosition = event.clientX - rect.left;
+      const yPosition = event.clientY - rect.top;
+      canvasContext.globalCompositeOperation = 'destination-out';
+      canvasContext.rect(xPosition, yPosition, scratchTraceHeight, scratchTraceWidth); 
+      canvasContext.fill();
+    }
+  };
+
+ useEffect(() => {
+    const canvas = canvasReference.current;
+    const canvasContext = canvas?.getContext('2d');
+
+    const giftBoxImage = new Image();
+    giftBoxImage.src = '/images/giftBox.png';
+    giftBoxImage.onload = () => {
+      canvasContext?.drawImage(giftBoxImage, 0, 0, canvas!.width, canvas!.height);
+    }
+  }, []);
+
+
+  return (
+    <GiftStyled className="gift" ref={giftReference}>
+      <canvas
+        ref={canvasReference}
+        className="gift__canvas"
+        width="300" 
+        height="300"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseUp} 
+      ></canvas>
+      <img className="gift__blowfish" src="/images/blowfish.png" />
+    </GiftStyled>
+  );
+};
+
+export default Gift;
--- src/components/GiftComponent/GiftStyled.ts
+++ src/components/GiftComponent/GiftStyled.ts
@@ -1,41 +1,27 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const GiftStyled = styled.div`
-display: flex;
-flex-direction: column;
-gap: 5px;
-position: absolute;
-width: 100%;
-height: 100%;
-
-
-.gift {
-
-  &__giftbox {
-  object-fit: cover;
-  object-fit: contain;
-  z-index: 1;
+  position: relative;
   width: 100%;
   height: 100%;
-  position: absolute;
 
-    &:active {
-    display:none
+  .gift {
+    &__canvas {
+      position: absolute;
+      top: -40px;
+      left: -50px;
+      z-index: 2; 
     }
 
+    &__blowfish {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1; 
+      width: 100%;
+      height: 100%;
+    }
   }
+`;
 
-  &__blowfish {
-    position: absolute;
-    left: 10px;
-    top: 30px;
-    object-fit: cover;
-    object-fit: contain;
-    z-index: 0;
-    width: 100%;
-  }
-}
-
-`
-
-export default GiftStyled
+export default GiftStyled;
